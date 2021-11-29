bd9c2d513425e61aa189435a4969377a410490a2
Aaron K <tonberryspawn@gmail.com>
2021-11-29 00:37:09 -0800

    minor addition of adding view maps route, cleaned up layout.scss

--- routes/maps.route.js
+++ routes/maps.route.js
@@ -5,12 +5,21 @@ const userDataHelper = require("../db-helper/user.helper");
 
 module.exports = (db) => {
   const { getUserWithUserId } = userDataHelper(db);
-  // creat map page
+
+  // view maps page
+  router.get("/", (req, res) => {
+    getUserWithUserId(req.session.userId).then((user) => {
+      res.render("maps", { user: user });
+    });
+  });
+
+  // create map page
   router.get("/new", (req, res) => {
     getUserWithUserId(req.session.userId).then((user) => {
       res.render("create_map", { user: user });
     });
   });
+
 
   // //@@ public route api/maps
   // //get all maps
