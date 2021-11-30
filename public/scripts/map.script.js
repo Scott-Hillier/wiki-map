3fff5fc08807448642de56dc3d77f53f823c4987
Aaron K <tonberryspawn@gmail.com>
2021-11-30 15:49:13 -0800

    minor note changes

--- public/scripts/map.script.js
+++ public/scripts/map.script.js
@@ -80,16 +80,16 @@ $(document).ready(() => {
 
     marker.bindPopup(popup).openPopup();
 
-    const point = {
-      mapId: 4,
-      title: "testing",
-      description: "test desc",
-      image: "url",
-      latitude: e.latlng.lat,
-      longitude: e.latlng.lng,
-      address: "test address",
-      type: "test type",
-    };
+    // const point = {
+    //   mapId: 4,
+    //   title: "testing",
+    //   description: "test desc",
+    //   image: "url",
+    //   latitude: e.latlng.lat,
+    //   longitude: e.latlng.lng,
+    //   address: "test address",
+    //   type: "test type",
+    // };
 
     // Ajax query to save the values:
     // $.ajax({
