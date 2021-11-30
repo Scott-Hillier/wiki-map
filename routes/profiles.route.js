6d9f6154807e1d06e69faa63c8b37ea372c85868
Scott Hillier <scotthhillier@gmail.com>
2021-11-29 17:10:03 -0800

    pulling master

--- routes/profiles.route.js
+++ routes/profiles.route.js
@@ -1,28 +1,58 @@
 const express = require("express");
 const router = express.Router();
 
+const userDataHelper = require("../db-helper/user.helper");
+// const mapDataHelper = require("../db-helper/map.helper");
+const profileDataHelper = require("../db-helper/map.helper");
+
 module.exports = (db) => {
-  //@@ private route api/profiles
-  //auth user favourites/contributes a map
-  router.post("/:userId/:mapId", (req, res) => {
-    getProfileMaps(req.session.userId, db).then((data) => res.json(data));
+  const { getUserWithUserId } = userDataHelper(db);
+  // const { getAllPublicMaps } = mapDataHelper(db);
+  const { getProfileMaps } = profileDataHelper(db);
+
+  //@@ PRIVATE /maps
+  // view maps page
+  router.get("/", (req, res) => {
+    getUserWithUserId(req.session.userId).then((user) => {
+      res.render("profile", { user: user });
+    });
   });
 
-  //@@ private route api/profiles
-  //get auth user's profile showing favourites and contributions
-  router.get("/:userId/favourites", (req, res) => {
-    getFavoriteProfileMaps(req.session.userid, isFavorite, db).then((data) =>
-      res.json(data)
-    );
+  //@@ PRIVATE /maps
+  // view create map page
+  router.get("/new", (req, res) => {
+    getUserWithUserId(req.session.userId).then((user) => {
+      res.render("create_map", { user: user });
+    });
   });
 
-  //@@ private route api/profiles
-  //get auth user's profile showing favourites and contributions
-  router.get("/:userId/contributions", (req, res) => {
-    getFavoriteProfileMaps(req.session.userid, isContributor, db).then((data) =>
-      res.json(data)
-    );
+  //@@ PUBLIC /maps
+  //get all public maps including user's maps if authenticated
+  router.get("/public-maps", (req, res) => {
+    getProfileMaps(req.session.userId).then((data) => res.json(data));
   });
+
+  // //@@ private route api/profiles
+  // //auth user favourites/contributes a map
+  // router.post("/:userId/:mapId", (req, res) => {
+  //   getProfileMaps(req.session.userId, db).then((data) => res.json(data));
+  // });
+
+  // //@@ private route api/profiles
+  // //get auth user's profile showing favourites and contributions
+  // router.get("/:userId/favourites", (req, res) => {
+  //   getFavoriteProfileMaps(req.session.userid, isFavorite, db).then((data) =>
+  //     res.json(data)
+  //   );
+  // });
+
+  // //@@ private route api/profiles
+  // //get auth user's profile showing favourites and contributions
+  // router.get("/:userId/contributions", (req, res) => {
+  //   getFavoriteProfileMaps(req.session.userid, isContributor, db).then((data) =>
+  //     res.json(data)
+  //   );
+  // });
 
   return router;
 };
--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users.route");
 const mapsRoutes = require("./routes/maps.route");
 // const pointsRoutes = require("./routes/points.route");
-// const profilesRoutes = require("./routes/profiles.route");
+const profilesRoutes = require("./routes/profiles.route");
 
 const userDataHelper = require("./db-helper/user.helper");
 const mapDataHelper = require("./db-helper/map.helper");
@@ -55,12 +55,12 @@ const { getAllPublicMaps } = mapDataHelper(db);
 app.use("/users", usersRoutes(db));
 app.use("/maps", mapsRoutes(db));
 // app.use("/api/points", pointsRoutes(db));
-// app.use("/api/profiles", profilesRoutes(db));
+app.use("/profile", profilesRoutes(db));
 
 // Home page
 app.get("/", (req, res) => {
   if (!req.session.userId) {
-    getAllPublicMaps().then((maps) => {
+    return getAllPublicMaps().then((maps) => {
       return res.render("index", { user: null, maps: maps });
     });
   }
